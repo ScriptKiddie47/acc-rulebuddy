6a2c53cdb31aaa11587f878e6061f37fa839fae9
paridhisaxena17 <81104395+paridhisaxena17@users.noreply.github.com>
2025-05-27 22:21:05 -0700

    Update E103.js

--- Rules/E103.js
+++ Rules/E103.js
@@ -1,6 +1,6 @@
-function evaluateE103Rule(request) {
-   // request AddVehicles_Request1.json
-    const data = request.data;
+function evaluateE103Rule(requestJson) {
+   // request AddSourceCoverageCdRequest.json
+    const data = requestJson.data;
 
     const programCdGvngLeg = data.programCdGvngLeg;
     const effectiveDt = new Date(data.effectiveDt);
@@ -37,3 +37,86 @@ function evaluateE103Rule(request) {
 
     return { ruleTriggered: false };
 }
+
+const requestJson = {
+  "data": {
+    "uniqueID": "1",
+    "drChgNwdSbUblHierarchyNbr": 0,
+    "action": "add",
+    "type": "Policy",
+    "programCdGvngLeg": "P55",
+    "reportingOfficeCdLeg": "084",
+    "policyNbr": "00A12675A",
+    "issuingBusinessUnitCdGvng": "14",
+    "brandCd": "BAP",
+    "coverageParts": [
+      {
+        "uniqueID": "2",
+        "drChgNwdSbUblHierarchyNbr": 0,
+        "dataTxt": "",
+        "action": "add",
+        "type": "AutoCoveragePart",
+        "industrySegmentTypeCdGvng": "18",
+        "programCdGvng": "P55",
+        "businessClassCdGvng": "P55",
+        "priorYrIssaCnt": 60000,
+        "covPartCd": "AU",
+        "govratingStateCd": "PA",
+        "coverages": [],
+        "linesOfInsurance": [],
+        "covPartStates": [
+          {
+            "type": "Coverage",
+            "sourceCoverageCd": "EMPHIRE"
+          }
+        ],
+        "Vehicles": [
+          {
+            "uniqueID": "9",
+            "action": "add",
+            "type": "Vehicle",
+            "coverages": [
+              {
+                "uarSequenceNbr": 1,
+                "classCode": "01121",
+                "stateCode": "PA",
+                "bureauVehicleClassIdCd": "LTRK",
+                "vehicleGroupTypeCd": "TTT",
+                "makeCd": "FORD",
+                "vehicleIdentificationNbr": "1FTNE24L09DA25896",
+                "makeNm": "FORD",
+                "commercialVehicleBusinessUseCd": "S",
+                "commercialVehicleBusinessSubUseCd": "01",
+                "vehicleClassification": {
+                  "drvChgWorkSubHierarchyNbr": 0,
+                  "type": "VehicleClassification",
+                  "selfPropelledVehicleInd": false,
+                  "pptLtrkVehicleUseTypeCd": "B"
+                },
+                "garagingStatePostalCd": "15211-4203"
+              }
+            ]
+          }
+        ],
+        "autoCovPartStates": [],
+        "coverageSymbolsGroups": []
+      }
+    ],
+    "selfPropelledVehicleCnt": 1,
+    "fleetInd": false,
+    "trailerCnt": 0,
+    "licensePlateCnt": 0,
+    "accountMultilineInd": true,
+    "drvAutoGaragingStates": [],
+    "totalDriverCnt": 1,
+    "namedInsured": {},
+    "locations": [],
+    "effectiveDt": "2025-07-14",
+    "formCd": "BA",
+    "expirationDt": "2025-07-14",
+    "transaction": {},
+    "businessClassCdGvng": "P55",
+    "policyID": 21567843
+  }
+};
+console.log(evaluateE103Rule(requestJson));
